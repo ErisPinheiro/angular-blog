61eb3d6524004af024fe26fdb2bc2cd59296fb7e
Eris Pinheiro <erisap@gmail.com>
2023-09-24 19:53:43 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title": 'Saiu o novo filme do Homem de Ferro',
+        "description": 'Marvel anuncia um novo filme do homem de ferro, estrelando atores consagrados pelo cinema de Hollywood. ',
+        "photoCover": "https://media.contentapi.ea.com/content/dam/news/www-ea/images/2022/09/ea-motive-new-title-teaser-16x9-featured.jpg.adapt.crop16x9.575p.jpg"
+    },
+    {
+        "id":"2",
+        "title": 'Saiu a nova série da Marvel no Disney+',
+        "description": 'Marvel anuncia uma nova serie do homem de ferro, ganahndo o amor do publico. ',
+        "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    }
+]
